38f2286c1039860fdc3e86f4ecba0dda11a63dc5
David Kapteyn <dave.kapteyn@gmail.com>
2021-10-25 14:38:47 -0600

    media query addded

--- time-tracking-dashboard-main/script.js
+++ time-tracking-dashboard-main/script.js
@@ -28,44 +28,44 @@ let dBlue = 'rgb(111, 118, 200)'
 
 
 //LOAD STATE
-getJSON('daily');
+getJSON('daily', "Yesterday ");
 dailyStats.style.color = 'white'
 
 
 //JSON DATA RETRIEVAL 
 
-async function getJSON(item){
+async function getJSON(item, lasttime){
     const response = await fetch('data.json');
     const data = await response.json();
     
   //WORK
 currentWorkHours.innerText = data[0].timeframes[item].current + 'hrs' ;
-previousWorkHours.innerText = 'Previous- ' + data[0].timeframes[item].previous + 'hrs';
+previousWorkHours.innerText = lasttime + "-"+ " " + data[0].timeframes[item].previous + 'hrs';
  //PLAY
  currentPlayHours.innerText = data[1].timeframes[item].current + 'hrs' ;
-previousPlayHours.innerText = 'Previous- ' + data[1].timeframes[item].previous + 'hrs';
+previousPlayHours.innerText = lasttime + "-"+ " " + data[1].timeframes[item].previous + 'hrs';
  //STUDY
  currentStudyHours.innerText = data[2].timeframes[item].current + 'hrs' ;
-previousStudyHours.innerText = 'Previous- ' + data[2].timeframes[item].previous + 'hrs';
+previousStudyHours.innerText = lasttime + "-"+ " " + data[2].timeframes[item].previous + 'hrs';
  //EXERCISE
  currentExerciseHours.innerText = data[3].timeframes[item].current + 'hrs' ;
-previousExerciseHours.innerText = 'Previous- ' + data[3].timeframes[item].previous + 'hrs';
+previousExerciseHours.innerText = lasttime + "-"+ " "+ data[3].timeframes[item].previous + 'hrs';
  //SOCIAL 
  currentSocialHours.innerText = data[4].timeframes[item].current + 'hrs' ;
-previousSocialHours.innerText = 'Previous- ' + data[4].timeframes[item].previous + 'hrs';
+previousSocialHours.innerText = lasttime + "-"+ " "+ data[4].timeframes[item].previous + 'hrs';
 
 //SELF CARE
 currentSelfCareHours.innerText = data[5].timeframes[item].current + 'hrs' 
-previousSelfCareHours.innerText = 'Previous- ' + data[5].timeframes[item].previous + 'hrs'
+previousSelfCareHours.innerText = lasttime + "-"+ " "+ data[5].timeframes[item].previous + 'hrs'
 
 
 }
 
 //ONCLICK FUNCTIONS FOR DAILY, WEEKLY, MONTHLY  
 
-document.getElementById('daily').addEventListener("click", () => getJSON("daily"))
-document.getElementById('weekly').addEventListener("click", () => getJSON("weekly"))
-document.getElementById('monthly').addEventListener("click", ()=> getJSON("monthly"))
+document.getElementById('daily').addEventListener("click", () => getJSON("daily", "Yesterday "))
+document.getElementById('weekly').addEventListener("click", () => getJSON("weekly", "Last Week "))
+document.getElementById('monthly').addEventListener("click", ()=> getJSON("monthly", "Last Month "))
 
 
 document.getElementById('daily').addEventListener("click", () => {dailyStats.style.color = 'white'; weeklyStats.style.color  = dBlue; monthlyStats.style.color = dBlue
